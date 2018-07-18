56158dce0d8db807d255bb7abfc90d832595a8b6
Djalma Manfrin <djalma.manfrin@rentcars.com>
2018-07-18 14:23:28 -0300

    remoção de código obsoleto

--- src/main/java/com/spark/App.java
+++ src/main/java/com/spark/App.java
@@ -1,10 +1,5 @@
 package com.spark;
 
-import static spark.Spark.*;
-import com.google.gson.Gson;
-import com.spark.beans.MyBean;
-import com.spark.utils.JsonResponse;
-
 /**
  * Hello world!
  *
@@ -16,20 +11,5 @@ public class App
     {
         new Config().setConfig();
         new Routes().setRoutes();
-//        before("/v1/*", (request, response) -> System.out.println("Passing before filter"));
-//        path("/v1", () -> {
-//            get("/:name", "application/json", (request, response)
-//                    -> new MyBean(request.params(":name")), new JsonResponse());
-//            post("/bean/new", "application/json", (request, response) -> {
-//                MyBean bean = new Gson().fromJson(request.body(), MyBean.class);
-//                System.out.println(bean.getName());
-//                System.out.println(bean.getAge());
-//                return "ok";
-//            }, new JsonResponse());
-//        });
-//        after("/v1/*", (request, response) -> {
-//            response.type("application/json");
-//        });
-//        afterAfter((request, response) -> System.out.println("Passing by After after filter"));
     }
 }
