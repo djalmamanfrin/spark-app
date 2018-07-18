80d6369ae914acb48016f500827e0572148fb6c5
Djalma Manfrin <djalma.manfrin@rentcars.com>
2018-07-18 11:18:27 -0300

    filters

--- src/main/java/com/spark/App.java
+++ src/main/java/com/spark/App.java
@@ -1,6 +1,7 @@
 package com.spark;
 
 import static spark.Spark.*;
+import com.google.gson.Gson;
 
 /**
  * Hello world!
@@ -11,8 +12,20 @@ public class App
 
     public static void main( String[] args )
     {
+        before("/v1/*", (request, response) -> System.out.println("Passing before filter"));
         path("/v1", () -> {
-            get("/:name", "application/json", (request, response) -> new MyBean(request.params(":name")), new JsonResponse());
+            get("/:name", "application/json", (request, response)
+                    -> new MyBean(request.params(":name")), new JsonResponse());
+            post("/bean/new", "application/json", (request, response) -> {
+                MyBean bean = new Gson().fromJson(request.body(), MyBean.class);
+                System.out.println(bean.getName());
+                System.out.println(bean.getAge());
+                return "ok";
+            }, new JsonResponse());
         });
+        after("/v1/*", (request, response) -> {
+            response.type("application/json");
+        });
+        afterAfter((request, response) -> System.out.println("Passing by After after filter"));
     }
 }
--- src/main/java/com/spark/MyBean.java
+++ src/main/java/com/spark/MyBean.java
@@ -2,12 +2,22 @@ package com.spark;
 
 public class MyBean {
     private String name;
+    private Integer age;
 
     MyBean(String name) {
         this.name = name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
     public String getName() {
         return name;
+    }
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+    public Integer getAge() {
+        return age;
     }
 }
